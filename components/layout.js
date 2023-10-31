c364a4de75649d6f11c9362831df3b85a6cc94e1
rwaddinsall <132865615+rwaddinsall@users.noreply.github.com>
2023-11-01 10:25:05 +1100

    changed meta

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Rhys Addinsall'
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Next.js Personal Website';
 
 // meta tags (like og:image), which are used to describe a page's content
 // Boolean home prop which will adjust the size of the title and the image
@@ -19,13 +19,12 @@ export default function Layout ({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Rhys Addinsall - Developer"
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={(siteTitle)}
+
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
